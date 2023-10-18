1829234aacf340979413f186f26fc5b9900e1f1b
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-10-18 12:59:38 -0600

    commit tree 334355ab73577e1c59a50e331a2c4f44c80a13be

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,63 @@
-# your code here
+class Stack
+    def initialize (limit=nil)
+        @stack=[]
+        @limit=limit
+    end
+
+    def push(value)
+       
+        if @limit.nil?|| @stack.length<@limit
+             @stack.push(value)
+        else
+            raise "Stack Overflow"
+        end 
+     
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+       @stack.last
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.length == 0
+    end
+    
+    def full?
+        @stack.length >= @limit
+    end
+
+    # def search(value)
+    #     if @stack.include?(value)
+    #     result = -1
+    #         if @stack.pop == value
+    #             return result
+    #         else
+    #             result += 1
+    #         end
+    #         result
+    #     else
+    #         return -1
+    #     end
+    # end
+
+    def search(value)
+        index = @stack.rindex(value)
+        if index
+          distance = @stack.length - index - 1
+          return distance
+        else
+          return -1
+      end
+    end
+
+end   
+
+
